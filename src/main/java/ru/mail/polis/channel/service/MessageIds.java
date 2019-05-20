42ad5bda8a2ab9e235db53c77258f6c5af7f6c7b
Dmitry Schitinin <dmitry.schitinin@corp.mail.ru>
2019-05-21 01:29:14 +0300

    MessageIds JavaDoc

--- src/main/java/ru/mail/polis/channel/service/MessageIds.java
+++ src/main/java/ru/mail/polis/channel/service/MessageIds.java
@@ -4,6 +4,16 @@ package ru.mail.polis.channel.service;
  * Generates "unique" ids for messages ;)
  */
 public class MessageIds {
+    /**
+     * Not them most successful implementation,
+     * even for non-distributed environment.
+     * Millisecond precision is very poor.
+     * <p>
+     * In production you must use something like
+     * https://en.wikipedia.org/wiki/Universally_unique_identifier
+     * or something from
+     * https://www.callicoder.com/distributed-unique-id-sequence-number-generator/
+     */
     public static long next() {
         return System.currentTimeMillis();
     }
